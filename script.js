e891bbf31364e017f169d6259bb1b8945ad7888a
Amin Amgad <96820371+aminamgad@users.noreply.github.com>
2023-06-19 13:25:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,94 @@
+let saturate = document.getElementById("saturate");
+let contrast = document.getElementById("contrast");
+let brightness = document.getElementById("brightness");
+let sepia = document.getElementById("sepia");
+let grayscale = document.getElementById("grayscale");
+let blurr = document.getElementById("blur");
+let hueRotate = document.getElementById("hue-rotate");
+
+let img = document.getElementById("img");
+let download = document.getElementById("download");
+let upload = document.getElementById("upload");
+
+let reset = document.querySelector("span");
+let imgBox = document.querySelector(".img-box");
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext('2d');
+
+
+
+function resetValue() {
+    img.style.filter='none';
+    saturate.value='100';
+    contrast.value='100';
+    brightness.value='100';
+    sepia.value='0';
+    grayscale.value='0';
+    blurr.value='0';
+    hueRotate.value='0';
+}
+
+window.onload = function(){
+    download.style.display="none";
+    reset.style.display="none";
+    imgBox.style.display="none";
+}
+
+upload.onchange = function () {
+    resetValue();
+    download.style.display="block";
+    reset.style.display="block";
+    imgBox.style.display="block";
+    let file = new FileReader();
+    file.readAsDataURL(upload.files[0]);
+    file.onload = function () {
+        img.src = file.result
+    }
+    img.onload = function() {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img,0,0,canvas.width,canvas.height);
+        img.style.display='none';
+    }
+}
+
+let filters = document.querySelectorAll("ul li input");
+
+filters.forEach(filter => {
+    filter.addEventListener('input' , function(){
+        ctx.filter = `
+            saturate(${saturate.value}%)
+            contrast(${contrast.value}%)
+            brightness(${brightness.value}%)
+            sepia(${sepia.value}%)
+            grayscale(${grayscale.value})
+            blur(${blurr.value}px)
+            hue-rotate(${hueRotate.value}deg)
+        `
+        ctx.drawImage(img,0,0,canvas.width,canvas.height);
+    })
+})
+
+reset.onclick = function name() {
+    resetValue();
+}
+
+// download.onclick = function() {
+//   var url = canvas.toDataURL(); // Replace with your file URL
+// //   var fileName = 'photo.jpg'; // Replace with the desired file name
+
+//   var link = document.createElement('a');
+//   link.href = url;
+// //   link.download = fileName;
+//   link.style.display = 'none';
+//   document.body.appendChild(link);
+//   link.click();
+//   document.body.removeChild(link);
+// }
+
+download.onclick = function () {
+    download.href = canvas.toDataURL();
+    download.style.color = '#fff';
+    download.style.textDecoration = 'none';
+}
